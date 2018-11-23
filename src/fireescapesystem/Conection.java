1015f93cf2dc491184ac10d12b6f15e327a9e0c0
martin <martin@localhost>
2018-11-23 21:20:28 +0100

    WIP GUI lines

--- src/fireescapesystem/Conection.java
+++ src/fireescapesystem/Conection.java
@@ -5,6 +5,11 @@
  */
 package fireescapesystem;
 
+import static fireescapesystem.Node.xSize;
+import static fireescapesystem.Node.ySize;
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
  * @author martin
@@ -12,7 +17,17 @@ package fireescapesystem;
 public class Conection {
     private Node destination;
     private double length;
+    private boolean chosen;
 
+    public boolean isChosen() {
+        return chosen;
+    }
+
+    public void setChosen(boolean chosen) {
+        this.chosen = chosen;
+    }
+    
+    
     public Conection(Node destination, double length) {
         this.destination = destination;
         this.length = length;
@@ -34,5 +49,32 @@ public class Conection {
         this.length = length;
     }
     
+    
+    public void Render(Graphics g, Node base) {
+        //xPosition += baseRenderPointX;
+        //yPosition += baseRenderPointY;
+        Color outline;
+        Color infill;
+        
+        if(chosen){
+        outline = Color.ORANGE;
+        infill = Color.red;
+        }else{
+        outline = Color.green;
+        infill = Color.darkGray;
+        
+        }
+        if(base.getxPosition()<destination.getxPosition()){
+        g.setColor(outline);
+        g.fillRect(base.getxPosition() + Node.xSize, base.getyPosition() + Node.ySize/4 , destination.getxPosition() + 3*Node.xSize/4, base.getyPosition() + 3*Node.ySize/4);
+        g.setColor(infill);
+        g.fillRect(base.getxPosition() + Node.xSize, 2 + base.getyPosition() + Node.ySize/4 , destination.getxPosition() + 3*Node.xSize/4, -2 + base.getyPosition() + 3*Node.ySize/4);
+        }    
+        
+        
+        //xPosition -= baseRenderPointX;
+        //yPosition -= baseRenderPointY;
+
+    }
     
 }
