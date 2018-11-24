ba39f3a7b730de605aece020d8d168d19c8952b1
martin <martin@localhost>
2018-11-24 07:27:44 +0100

    WIP GUI lines

--- src/fireescapesystem/Pathfinder.java
+++ src/fireescapesystem/Pathfinder.java
@@ -37,55 +37,8 @@ public class Pathfinder {
         this.conections = conections;
     }
 
-    public void seek(Node a) {
-        System.out.println("Actual: " + a.getID());
-        ArrayList<Conection> valid = new ArrayList<Conection>();
-        for (Conection c : conections) {
-            if (c.getBase() == a || a == c.getDestination()) {
-                valid.add(c);
-
-            }
-
-        }
-        
-        for(Conection c : valid){
-        Node tmp;
-            if(c.getBase() == a){
-        //c.getDestination().setSUM(a.getSUM() + c.getLength());
-        tmp = c.getDestination();
-        }else{
-        //c.getDestination().setSUM(a.getSUM() + c.getLength());
-        tmp = c.getBase();
-            }
-         if(((tmp.isIsExit() == false) && (tmp.getSUM()> a.getSUM() + c.getLength())) || (tmp.isIsExit() == false) && tmp.getSUM()==-1){
-             {tmp.setSUM(a.getSUM() + c.getLength());
-         if(a==c.getBase()){
-         c.setWayB2D(false);
-         seek(tmp);
-         }
-         
-             }
-         
-         }
-            
-        
-        }
-    }
     
-    public void findAndMark() {
-        ArrayList<Node> exits = new ArrayList<Node>();
-        for (Node n : nodes) {
-            if (n.isIsExit()) {
-                exits.add(n);
-            }
-        }
-
-        for (Node e : exits) {
-            e.setSUM(0);
-            System.out.println("Exit found :" + e.getID());
-            seek(e);
-        }
-
-    }
+    
+    
 
 }
